f78c3c0c44e6b706bc62785468315f42cc128235
应佳伟 <yingjiawei@junleizg.com>
2018-04-28 15:58:40 +0800

    format api_interface

--- lib/model/api/api_interface.dart
+++ lib/model/api/api_interface.dart
@@ -4,25 +4,6 @@ import 'package:http/http.dart' as http;
 import 'package:jkd_flutter/utils/sp_utils.dart';
 
 class API {
-  static Future<http.Response> sms(String mobile) async {
-    print("""
-            {
-  	              "biz_content": {
-  		              "mobile": "$mobile"
-  	              }
-            }
-            """);
-    var response = await http.post(sms_url, headers: public_header, body: """
-            {
-  	              "biz_content": {
-  		              "mobile": "$mobile"
-  	              }
-            }
-            """);
-    print(response.body);
-    return response;
-  }
-
   static const base_url = 'http://api.junleizg.com.cn';
   static const access_token = 'access_token';
   static const profile_url = base_url + '/v1/user/profile';
@@ -32,64 +13,72 @@ class API {
   static const Map<String, String> public_header = {
     'Content-Type': 'application/json; charset=utf-8'
   };
-  static Future<http.Response> login(String mobile, String code) async {
-    print(public_header);
-    print("""
+  static Future<http.Response> sms(String mobile) async {
+    var reqBody = """
             {
-	              "biz_content": {
-		              "mobile": "$mobile",
-  		              "code": "$code"
+  	              "biz_content": {
+  		              "mobile": "$mobile"
   	              }
             }
-            """);
-    var response = await http.post(login_url, headers: public_header, body: """
+            """;
+    print(public_header);
+    print(sms_url);
+    print(reqBody);
+    var response =
+        await http.post(sms_url, headers: public_header, body: reqBody);
+    print(response.body);
+    return response;
+  }
+
+  static Future<http.Response> login(String mobile, String code) async {
+    var reqBody = """
             {
   	              "biz_content": {
   		              "mobile": "$mobile",
   		              "code": "$code"
   	              }
             }
-            """);
+            """;
+    print(public_header);
+    print(login_url);
+    print(reqBody);
+    var response =
+        await http.post(login_url, headers: public_header, body: reqBody);
     print(response.body);
 
     return response;
   }
 
   static Future<http.Response> application() async {
-    print(public_header);
     var token = await SPUtils.get(access_token);
-    print("""
+    var reqBody = """
             {
 	              "access_token": "$token"
             }
-            """);
+            """;
+    print(public_header);
+    print(application_url);
+    print(reqBody);
     var response =
-        await http.post(application_url, headers: public_header, body: """
-            {
-	              "access_token": "$token"
-            }
-            """);
+        await http.post(application_url, headers: public_header, body: reqBody);
     print(response.body);
 
     return response;
   }
 
   static Future<http.Response> profile() async {
-    print(public_header);
     var token = await SPUtils.get(access_token);
-    print("""
+    var reqBody = """
             {
 	              "access_token": "$token"
             }
-            """);
+            """;
+    print(public_header);
+    print(profile_url);
+    print(reqBody);
     var response =
-        await http.post(profile_url, headers: public_header, body: """
-            {
-	              "access_token": "$token"
-            }
-            """);
+        await http.post(profile_url, headers: public_header, body: reqBody);
     print(response.body);
-
     return response;
   }
 }
